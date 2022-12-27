26e4f93ff9ebbc61a496637087c179bdfa9f660d
Chuck Belcher <chuckbelcher@Chucks-MacBook-Pro.local>
2022-12-27 16:16:58 -0500

    Added database file / functions to add / get / display data

--- section-3/main.py
+++ section-3/main.py
@@ -1,18 +1,33 @@
-menu = """Please select one of the following options: 
+from database import add_entry, get_entries
+
+menu = """\nPlease select one of the following options: 
  1) Add new entry
  2) View entries
  3) Exit
 
 Your selection: """
 
-welcome_message = "Welcome to the programming diary\n"
+welcome_message = "Welcome to the programming diary"
+
+def new_entry():
+    entry_content = input('What have you learned? ')
+    entry_date = input("Enter date of learning: ")
+    add_entry(entry_content, entry_date)
+    
+
+def view_entries(entries):
+    print(f"Here are the {len(entries)} etries you asked for\n")
+    for entry in entries:
+        print(f"{entry['date']} - {entry['content']}")
 
 print(welcome_message)
 
 while (user_selection := input(menu)) !="3":
     if user_selection == "1":
-        print("You want to add a new entry")
-    elif user_selection == "2":
-        print("You want to view the entries")
+        new_entry()
+    
+    elif user_selection == "2":        
+        view_entries(get_entries())
+        
     else:
         print("You pressed an invalid option")
